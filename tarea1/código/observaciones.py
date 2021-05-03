936b02f7292fa934886e11f452cea3584d4dfec7
camilo-nb <69209318+camilo-nb@users.noreply.github.com>
2021-05-03 05:52:30 -0400

    Add Tmax gaussian fit

--- tarea1/código/observaciones.py
+++ tarea1/código/observaciones.py
@@ -40,6 +40,7 @@ plt.show()
 def gaussian(x, height, center, width):
     return height * np.exp(-0.5 * ((x - center) / width) ** 2)
 
+Tmax = np.zeros((5, 3))
 for j in range(3):
     fig, axs = plt.subplots(3, 3, sharex=True, sharey=True, figsize=(3.25, 3.25))
     axs = axs.flatten()
@@ -53,6 +54,58 @@ for j in range(3):
         popt, pcov = scipy.optimize.curve_fit(gaussian, v[5*j+i], T[5*j+i], p0=[20, 10, 1])
         axs[n-1].plot(v[5*j+i], T[5*j+i], color="green", drawstyle="steps-mid", linestyle="-", linewidth=0.5)
         axs[n-1].plot(v[5*j+i], gaussian(v[5*j+i], *popt), color="red", drawstyle="default", linestyle="-", linewidth=0.5)
+        Tmax[i, j] = T[5*j+i].max()
+    axs[8-1].set_xlabel(r"Velocidad [km/s]")
+    axs[4-1].set_ylabel(r"Temperatura [K]")
     plt.subplots_adjust(wspace=0, hspace=0)
     fig.savefig("../informe/specfit{}.pdf".format(j+1))
     plt.show()
+
+Tmax = Tmax.mean(axis=1)
+fig = plt.figure(figsize=(3.25, 3.25))
+
+# https://doi.org/10.1109/MSP.2011.941846
+p = np.poly1d(np.polyfit(lii[0:5][[1, 2, 3]], np.log(Tmax[[1, 2, 3]]), 2))
+heightlii = np.exp(p.c[2]-p.c[1]*p.c[1]/(4*p.c[0]))
+centerlii = -p.c[1]/(2*p.c[0])
+widthlii = np.sqrt(-1/(2*p.c[0]))
+
+xlii = np.linspace(lii.min()-0.075, lii.max()+0.075)
+ylii = gaussian(xlii, heightlii, centerlii, widthlii)
+
+axlii = fig.add_subplot(111, label=r"$l^{II}$")
+axlii.plot(xlii, ylii, color="red")
+for x in lii[0:5][[1, 2, 3]]:
+    axlii.axvline(x, color="darkred", linestyle="--", linewidth=0.5)
+axlii.set_xlabel(r"$l^{II} [\degree]$", color="red")
+axlii.set_ylabel(r"$T_{max} [K]$", color="red")
+axlii.tick_params(axis='x', colors="red")
+axlii.tick_params(axis='y', colors="red")
+axlii.yaxis.set_tick_params(rotation=90)
+axlii.tick_params(direction="in")
+
+p = np.poly1d(np.polyfit(bii[0:5][[0, 2, 4]], np.log(Tmax[[0, 2, 4]]), 2))
+heightbii = np.exp(p.c[2]-p.c[1]*p.c[1]/(4*p.c[0]))
+centerbii = -p.c[1]/(2*p.c[0])
+widthbii = np.sqrt(-1/(2*p.c[0]))
+xbii = np.linspace(bii.min()-0.075, bii.max()+0.075)
+ybii = gaussian(xbii, heightbii, centerbii, widthbii)
+axbii = fig.add_subplot(111, label=r"$b^{II}$", frame_on=False)
+axbii.plot(xbii, ybii, color="green")
+for x in bii[0:5][[0, 2, 4]]:
+    axbii.axvline(x, color="darkgreen", linestyle=":", linewidth=0.5)
+axbii.xaxis.tick_top()
+axbii.yaxis.tick_right()
+axbii.set_xlabel(r"$b^{II} [\degree]$", color="green")
+ylabelaxbii = axbii.set_ylabel(r"$T_{max}$ [K]", color="green")
+ylabelaxbii.set_rotation(-90)
+axbii.xaxis.set_label_position('top') 
+axbii.yaxis.set_label_position('right') 
+axbii.tick_params(axis='x', colors="green")
+axbii.tick_params(axis='y', colors="green")
+axbii.yaxis.set_tick_params(rotation=-90)
+axbii.yaxis.set_label_coords(1.11, 0.5)
+axbii.tick_params(direction="in")
+
+fig.savefig("../informe/tmax.pdf")
+plt.show()

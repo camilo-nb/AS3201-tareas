93a62ffa51163068f0380c2f98c9ef8a379bacdf
camilo-nb <69209318+camilo-nb@users.noreply.github.com>
2021-05-23 01:20:50 -0400

    Fix crval in get_values_from(header, axis)

--- tarea2/código/curvaderotacion.py
+++ tarea2/código/curvaderotacion.py
@@ -6,7 +6,7 @@ def get_values_from(header: fits.header.Header, axis: int) -> np.ndarray:
     """Get real coordinate values from `header` at `axis` instead of pixels."""
     naxis = header[f"NAXIS{axis}"]  # number of pixels on `axis`
     crpix = header[f"CRPIX{axis}"]  # reference pixel for `axis`
-    crval = header[f"NAXIS{axis}"]  # coordinate value at `crpix`
+    crval = header[f"CRVAL{axis}"]  # coordinate value at `crpix`
     cdelt = header[f"CDELT{axis}"]  # pixel spacing for `axis`
     return crval + (1 - crpix + np.arange(naxis)) * cdelt
 
@@ -15,6 +15,6 @@ cube = fits.open("../southgal_fixbadc.fits")
 data = cube[0].data
 header = cube[0].header
 
-vel = get_values_from(header, 1)
-lon = get_values_from(header, 2)
-lat = get_values_from(header, 3)
+vel = get_values_from(header, 1)  # VELO-LSR
+lon = get_values_from(header, 2)  # GLON-FLT
+lat = get_values_from(header, 3)  # GLAT-FLT

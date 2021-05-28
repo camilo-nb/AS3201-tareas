6709c0bb5de566130ef0e7071a16d983ef854be8
camilo-nb <69209318+camilo-nb@users.noreply.github.com>
2021-05-27 23:06:41 -0400

    Add rotation curve script

--- tarea2/código/curvaderotacion.py
+++ tarea2/código/curvaderotacion.py
@@ -1,6 +1,8 @@
 import numpy as np
 from astropy.io import fits
-
+from astropy.stats import SigmaClip, sigma_clip
+import matplotlib.pyplot as plt
+from astropy import units as u
 
 def get_values_from(header: fits.header.Header, axis: int) -> np.ndarray:
     """Get real coordinate values from `header` at `axis` instead of pixels."""
@@ -12,9 +14,54 @@ def get_values_from(header: fits.header.Header, axis: int) -> np.ndarray:
 
 
 cube = fits.open("../southgal_fixbadc.fits")
-data = cube[0].data
+data = cube[0].data  # indexing: [galactic latitude, galactic longitude, velocity]
 header = cube[0].header
 
-vel = get_values_from(header, 1)  # VELO-LSR
-lon = get_values_from(header, 2)  # GLON-FLT
-lat = get_values_from(header, 3)  # GLAT-FLT
+v = get_values_from(header, 1)  # VELO-LSR
+l = get_values_from(header, 2)  # GLON-FLT
+b = get_values_from(header, 3)  # GLAT-FLT
+
+v_maximorum = np.zeros_like(l)
+b_v_maximorum = np.zeros_like(l)
+
+sigclip = SigmaClip(sigma_upper=5, sigma_lower=np.inf, maxiters=1, cenfunc=np.mean, stdfunc=np.std)
+filtered_data = sigclip(data, axis=2)
+iv = filtered_data.mask.argmax(axis=2)
+for il in range(len(l)):
+    v_tan = np.zeros_like(b)
+    for ib in range(len(b)):
+        i = iv[ib][il]
+        v_tan[ib] = v[i] if i else np.nan
+    i = np.nanargmin(v_tan)
+    v_maximorum[il] = v_tan[i]
+    b_v_maximorum[il] = b[i]
+
+R0 = 8.5*u.kpc
+v0 = 220*u.km/u.s
+w0 = v0/R0.to(u.km)
+
+lrad = l*np.pi/180.
+vtan = v0*abs(np.sin(l*np.pi/180.))+v_maximorum*np.sign(np.sin(l*np.pi/180.))*u.km/u.s
+
+R = abs(R0*np.sin(l*np.pi/180.))
+w = w0+vtan/R.to(u.km)
+
+plt.rcParams.update({'font.size': 7})
+fig, ax = plt.subplots(figsize=(3.25, 3.25))
+ax.plot(R, vtan, c='r', lw=1)
+ax.set_xlabel(r"$R_{\odot}\sin\,l$ [kpc]")
+ax.set_ylabel(r"$v_\tan$ [km/s]")
+ax.yaxis.set_tick_params(rotation=90)
+ax.tick_params(direction="in", top=True, right=True)
+fig.savefig("../informe/rsc/vtan.pdf")
+plt.show()
+
+plt.rcParams.update({'font.size': 7})
+fig, ax = plt.subplots(figsize=(3.25, 3.25))
+ax.plot(R, w, c='r', lw=1)
+ax.set_xlabel(r"$R_{\odot}\sin\,l$ [kpc]")
+ax.set_ylabel(r"$\omega$ [rad/s]")
+ax.yaxis.set_tick_params(rotation=90)
+ax.tick_params(direction="in", top=True, right=True)
+fig.savefig("../informe/rsc/w.pdf")
+plt.show()

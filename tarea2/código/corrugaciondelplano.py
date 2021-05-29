5e4b77d11ba9a00a24e78c358ca191e8d8163fb0
camilo-nb <69209318+camilo-nb@users.noreply.github.com>
2021-05-29 18:47:34 -0400

    Add corrugation script

---
+++ tarea2/código/corrugaciondelplano.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from curvaderotacion import R0, R, l, b_v_maximorum
+
+def Z(l: float, b: float) -> float:
+    return R0*np.cos(l*np.pi/180.)*np.tan(b*np.pi/180.)
+
+plt.rcParams.update({'font.size': 7})
+fig, ax = plt.subplots(figsize=(3.25, 3.25))
+ax.plot(-R, Z(l, b_v_maximorum), c='k', lw=0.5)
+ax.set_xlabel(r"$-R_{\odot}\sin\,l$ [kpc]")
+ax.set_ylabel(r"$Z$ [kpc]")
+ax.yaxis.set_tick_params(rotation=90)
+ax.tick_params(direction="in", top=False, right=True)
+axl = ax.secondary_xaxis("top", functions=(lambda R: (180./np.pi*np.arcsin(-R/R0.value)-360)%360, lambda l: -R0.value*np.sin(l*np.pi/180.)))
+axl.set_xlabel(r"$l$ [°]")
+axl.tick_params(direction="in", top=True)
+fig.savefig("../informe/rsc/Z.pdf")
+plt.show()

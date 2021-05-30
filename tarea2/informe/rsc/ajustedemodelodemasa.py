39cb2f04a0f6603cc25926d2273e46056deb0c2b
camilo-nb <69209318+camilo-nb@users.noreply.github.com>
2021-05-30 03:51:18 -0400

    Add appendix

---
+++ tarea2/informe/rsc/ajustedemodelodemasa.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy import units as u
+from scipy.optimize import curve_fit
+
+from curvaderotacion import R, vrot
+
+G = 4.302e-6*u.kpc*u.km**2/u.s**2/u.Msun
+
+vrot = -vrot.value
+R = -R.value
+
+def vel(R: float, M: float) -> float:
+    return np.sqrt(G.value*M/R)
+
+def pointlike_mass_model(M0: float) -> float:
+    """`M0` point mass in the center of the Galaxy."""
+    return M0
+
+def uniform_disk_mass_model(R: float, s: float) -> float:
+    """`s` uniform superficial mass density."""
+    return np.pi*R**2*s
+
+def uniform_sphere_mass_model(R: float, rho: float) -> float:
+    """`rho` uniform volumetric mass density."""
+    return 4/3*np.pi*R**3*rho
+
+def uniform_disk_and_pointlike_mass_model(R: float, s: float, M0: float) -> float:
+    return uniform_disk_mass_model(R, s) + pointlike_mass_model(M0)
+
+def uniform_sphere_and_pointlike_mass_model(R: float, rho: float, M0: float) -> float:
+    return uniform_sphere_mass_model(R, rho) + pointlike_mass_model(M0)
+
+popt_point, pcov_point = curve_fit(lambda R, M0: vel(R, pointlike_mass_model(M0)),
+                                   R, vrot, check_finite=False)
+perr_point = np.sqrt(np.diag(pcov_point))
+popt_disk, pcov_disk = curve_fit(lambda R, s: vel(R, uniform_disk_mass_model(R, s)),
+                                 R, vrot, check_finite=False)
+perr_disk = np.sqrt(np.diag(pcov_disk))
+popt_sphere, pcov_sphere = curve_fit(lambda R, rho: vel(R, uniform_sphere_mass_model(R, rho)),
+                                     R, vrot, check_finite=False)
+perr_sphere = np.sqrt(np.diag(pcov_sphere))
+popt_disk_point, pcov_disk_point = curve_fit(lambda R, s, M0: vel(R, uniform_disk_and_pointlike_mass_model(R, s, M0)),
+                                             R, vrot, check_finite=False)
+perr_disk_point = np.sqrt(np.diag(pcov_disk_point))
+popt_sphere_point, pcov_sphere_points = curve_fit(lambda R, rho, M0: vel(R, uniform_sphere_and_pointlike_mass_model(R, rho, M0)),
+                                                  R, vrot, check_finite=False)
+perr_sphere_point = np.sqrt(np.diag(pcov_sphere_points))
+
+print("M0 =", popt_point, "+-", perr_point, "RMS =",
+      np.sqrt(np.mean((vrot-vel(R, pointlike_mass_model(*popt_point)))**2)))
+print("s0 =", popt_disk, "+-", perr_disk, "RMS =",
+      np.sqrt(np.mean((vrot-vel(R, uniform_disk_mass_model(R, *(popt_disk+perr_disk))))**2)))
+print("rho0 =", popt_sphere, "+-", perr_sphere, "RMS =",
+      np.sqrt(np.mean((vrot-vel(R, uniform_sphere_mass_model(R, *popt_sphere)))**2)))
+print("s0 M0 =", popt_disk_point, "+-", perr_disk_point,
+      "RMS =", np.sqrt(np.mean((vrot-vel(R, uniform_disk_and_pointlike_mass_model(R, *popt_disk_point)))**2)))
+print("rho0 M0 =", popt_sphere_point, "+-", perr_sphere_point,
+      "RMS =", np.sqrt(np.mean((vrot-vel(R, uniform_sphere_and_pointlike_mass_model(R, *popt_sphere_point)))**2)))
+
+plt.rcParams.update({'font.size': 7})
+fig, ax = plt.subplots(figsize=(3.25, 3.25))
+ax.plot(R, -vrot,
+        c='k', lw=0.25, marker='s', markersize=1, mfc="none", markeredgewidth=0.25, label="Datos")
+ax.plot(R, -vel(R, pointlike_mass_model(*popt_point)),
+        c="tab:red", lw=0.5, label="Punto")
+ax.fill_between(R, -vel(R, pointlike_mass_model(*(popt_point+perr_point))),
+                -vel(R, pointlike_mass_model(*(popt_point-perr_point))),
+                facecolor="tab:red", alpha=0.25)
+ax.plot(R, -vel(R, uniform_sphere_and_pointlike_mass_model(R, *popt_sphere_point)),
+        c="tab:purple", lw=0.5, label="Esfera + Punto")
+ax.fill_between(R, -vel(R, uniform_sphere_and_pointlike_mass_model(R, *(popt_sphere_point+perr_sphere_point))),
+                -vel(R, uniform_sphere_and_pointlike_mass_model(R, *(popt_sphere_point-perr_sphere_point))),
+                facecolor="tab:purple", alpha=0.25)
+ax.plot(R, -vel(R, uniform_sphere_mass_model(R, *popt_sphere)),
+        c="tab:orange", lw=0.5, label="Esfera")
+ax.fill_between(R, -vel(R, uniform_sphere_mass_model(R, *(popt_sphere+perr_sphere))),
+                -vel(R, uniform_sphere_mass_model(R, *(popt_sphere-perr_sphere))),
+                facecolor="tab:orange", alpha=0.25)
+ax.plot(R, -vel(R, uniform_disk_and_pointlike_mass_model(R, *popt_disk_point)),
+        c="tab:blue", lw=0.5, label="Disco + Punto")
+ax.fill_between(R, -vel(R, uniform_disk_and_pointlike_mass_model(R, *(popt_disk_point+perr_disk_point))),
+                -vel(R, uniform_disk_and_pointlike_mass_model(R, *(popt_disk_point-perr_disk_point))),
+                facecolor="tab:blue", alpha=0.25)
+ax.plot(R, -vel(R, uniform_disk_mass_model(R, *popt_disk)),
+        c="tab:green", lw=0.5, label="Disco")
+ax.fill_between(R, -vel(R, uniform_disk_mass_model(R, *(popt_disk+perr_disk))),
+                -vel(R, uniform_disk_mass_model(R, *(popt_disk-perr_disk))),
+                facecolor="tab:green", alpha=0.25)
+ax.legend(loc="upper right")
+ax.set_xlabel(r"$-R_{\odot}\sin\,l$ [kpc]")
+ax.set_ylabel(r"$v_\mathrm{rot}$ [km/s]")
+ax.yaxis.set_tick_params(rotation=90)
+ax.tick_params(direction="in", top=True, right=True)
+fig.savefig("../informe/rsc/massmodels.pdf")
+plt.show()
---
+++ tarea2/informe/rsc/curvaderotacion.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import numpy as np
+from astropy.io import fits
+from astropy.stats import SigmaClip
+import matplotlib.pyplot as plt
+from astropy import units as u
+
+R0 = 8.5*u.kpc
+v0 = 220*u.km/u.s
+
+def get_values_from(header: fits.header.Header, axis: int) -> np.ndarray:
+    """Get real coordinate values instead of pixels from `header` at `axis`."""
+    naxis = header[f"NAXIS{axis}"]  # number of pixels on `axis`
+    crpix = header[f"CRPIX{axis}"]  # reference pixel for `axis`
+    crval = header[f"CRVAL{axis}"]  # coordinate value at `crpix`
+    cdelt = header[f"CDELT{axis}"]  # pixel spacing for `axis`
+    return crval + (1 - crpix + np.arange(naxis)) * cdelt
+
+cube = fits.open("../southgal_fixbadc.fits")
+data = cube[0].data  # indexing: [galactic latitude, galactic longitude, velocity]
+header = cube[0].header
+
+v = get_values_from(header, 1)  # VELO-LSR
+l = get_values_from(header, 2)  # GLON-FLT
+b = get_values_from(header, 3)  # GLAT-FLT
+
+v_maximorum = np.zeros_like(l)
+b_v_maximorum = np.zeros_like(l)
+
+sigclip = SigmaClip(sigma_lower=3.5, sigma_upper=3.5, maxiters=None, cenfunc=np.mean, stdfunc=np.std)
+T_noise = sigclip(data, axis=2)
+rms = np.sqrt(np.mean(T_noise**2, axis=2))
+T_rms = np.repeat(rms[..., np.newaxis], len(v), axis=2)
+iv_terminal = (data > 5*T_rms).argmax(axis=2)
+for il in range(len(l)):
+    v_terminal = np.zeros_like(b)
+    for ib in range(len(b)):
+        i = iv_terminal[ib][il]
+        v_terminal[ib] = v[i] if i else np.nan
+    i = np.nanargmin(v_terminal)
+    v_maximorum[il] = v_terminal[i]
+    b_v_maximorum[il] = b[i]
+
+vrot = v0*np.sin(l*np.pi/180.)+v_maximorum*u.km/u.s
+R = R0*np.sin(l*np.pi/180.)
+
+if __name__ == "__main__":
+    
+    plt.rcParams.update({'font.size': 7})
+    fig, ax = plt.subplots(figsize=(3.25, 3.25))
+    ax.plot(v, data[14][115], c='k', lw=0.25, marker='s', markersize=1, mfc="none", markeredgewidth=0.25)
+    ax.axhline(5*rms[14][115], c='k', lw=0.25, ls="--")
+    ax.axvline(v[iv_terminal[14][115]], c='k', lw=0.25, ls="--")
+    ax.set_xlabel(r"$v_\mathrm{LSR}$ [km/s]")
+    ax.set_ylabel(r"$T$ [K]")
+    ax.text(0.1, 0.9, f"$l=${l[115]:.3f}", ha='left', va='center', transform=ax.transAxes)
+    ax.text(0.1, 0.85, f"$b=${b[14]:.3f}", ha='left', va='center', transform=ax.transAxes)
+    ax.text(0.1, 0.8, r"$v_\mathrm{term}=$"+f"{v[iv_terminal[14][115]]:.3f}", ha='left', va='center', transform=ax.transAxes)
+    ax.yaxis.set_tick_params(rotation=90)
+    ax.tick_params(direction="in", top=True, right=True)
+    fig.savefig("../informe/rsc/vterminal.pdf")
+    plt.show()
+    
+    w0 = v0/R0.to(u.km)
+
+    w = w0+(v_maximorum*u.km/u.s)/R.to(u.km)
+
+    plt.rcParams.update({'font.size': 7})
+    fig, ax = plt.subplots(figsize=(3.25, 3.25))
+    ax.plot(-R, vrot, c='k', lw=0.25, marker='s', markersize=1, mfc="none", markeredgewidth=0.25)
+    ax.set_xlabel(r"$-R_{\odot}\sin\,l$ [kpc]")
+    ax.set_ylabel(r"$v_\mathrm{rot}$ [km/s]")
+    ax.yaxis.set_tick_params(rotation=90)
+    ax.tick_params(direction="in", top=False, right=True)
+    axl = ax.secondary_xaxis("top", functions=(lambda R: (180./np.pi*np.arcsin(-R/R0.value)-360)%360, lambda l: -R0.value*np.sin(l*np.pi/180.)))
+    axl.set_xlabel(r"$l$ [$^{\circ}$]")
+    axl.tick_params(direction="in", top=True)
+    fig.savefig("../informe/rsc/vrot.pdf")
+    plt.show()
+
+    plt.rcParams.update({'font.size': 7})
+    fig, ax = plt.subplots(figsize=(3.25, 3.25))
+    ax.plot(-R, w, c='k', lw=0.25, marker='s', markersize=1, mfc="none", markeredgewidth=0.25)
+    ax.set_xlabel(r"$-R_{\odot}\sin\,l$ [kpc]")
+    ax.set_ylabel(r"$\omega$ [rad/s]")
+    ax.yaxis.set_tick_params(rotation=90)
+    ax.tick_params(direction="in", top=False, right=True)
+    axl = ax.secondary_xaxis("top", functions=(lambda R: (180./np.pi*np.arcsin(-R/R0.value)-360)%360, lambda l: -R0.value*np.sin(l*np.pi/180.)))
+    axl.set_xlabel(r"$l$ [$^{\circ}$]")
+    axl.tick_params(direction="in", top=True)
+    fig.savefig("../informe/rsc/w.pdf")
+    plt.show()
